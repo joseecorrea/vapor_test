1727f9e8ef875a217f95a553b6a87193b9127a0f
Jose Correa <josecorreadev@gmail.com>
2022-12-08 21:21:52 +0000

    Add enum for the operations that Calculator controller may perform

---
+++ Sources/App/OperationsEnum.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jose E on 8/12/22.
+//
+
+import Foundation
+
+enum Operations {
+    case Addition
+    case Substraction
+    case Multiplication
+    case Division
+    case SquareRoot
+}
